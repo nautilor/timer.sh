6339d9a3ec51257d0a0d07e0c408fd74054e08bb
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2022-01-28 17:40:44 +0100

    Create timer.sh

---
+++ timer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function refresh() {
+    # move cursor to 0,0
+    printf '\e[H'
+    # clear the screen
+    printf '\e[2J'
+    # set max scroll to 7 lines
+    printf '\e[1;8r'
+    # hide the cursor
+    printf '\e[?25l'
+}
+
+# used to make the timer start from 00:00:00
+start=$(date +%s)
+refresh
+# do not mess up the text if the window get resized
+trap 'refresh' WINCH
+# run forever
+while true; do
+    # print the time without appending a new line every time
+    figlet -w $(tput cols) -c "$(date -u --date @$(( $(date +%s) - $start )) +%H:%M:%S)"
+    sleep 1
+done
